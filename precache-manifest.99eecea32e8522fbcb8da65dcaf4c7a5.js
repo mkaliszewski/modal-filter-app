602e4c7576498b51c7a2fa617f1c9780efda14db
mikibaka <kaliszewski.mikolaj@gmail.com>
2020-10-11 22:31:49 +0200

    Updates

---
+++ precache-manifest.99eecea32e8522fbcb8da65dcaf4c7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acca8fdf056b0ba540c8d000528283a",
+    "url": "/modal-filter-app/index.html"
+  },
+  {
+    "revision": "74a408f1b410e608143a",
+    "url": "/modal-filter-app/static/css/main.95082744.chunk.css"
+  },
+  {
+    "revision": "fe6fb302e500b376c967",
+    "url": "/modal-filter-app/static/js/2.ad250886.chunk.js"
+  },
+  {
+    "revision": "8bcdfbedbaedaebc1be8002ea83beeb7",
+    "url": "/modal-filter-app/static/js/2.ad250886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a408f1b410e608143a",
+    "url": "/modal-filter-app/static/js/main.9a266e8c.chunk.js"
+  },
+  {
+    "revision": "7ab95981f81d6c40f3b7",
+    "url": "/modal-filter-app/static/js/runtime-main.c65d3c74.js"
+  }
+]);
